e85e71b94b5c874f5a71b6abc7520079365af583
Valorless <Minac2503@hotmail.dk>
2026-04-03 17:02:25 +0200

    Interactive bag info and message helpers
    Refactor CommandInfo to use the Player sender and send interactive chat components for bag details (UUID, texture, owner, creator, etc.) with hover text and copy-to-clipboard actions; collect info into a list and print formatted lines. Extend Message with new constructors and helpers (typed ChatColor, hover setter, click/suggest/open/copy events) and adjust Send behavior. Bump build number to 2783.

--- src/main/java/valorless/havenbags/commands/CommandInfo.java
+++ src/main/java/valorless/havenbags/commands/CommandInfo.java
@@ -5,102 +5,147 @@ import java.util.List;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import net.md_5.bungee.api.ChatMessageType;
 import valorless.havenbags.BagData;
 import valorless.havenbags.datamodels.Data;
+import valorless.havenbags.datamodels.Message;
 import valorless.havenbags.enums.BagState;
 import valorless.havenbags.persistentdatacontainer.PDC;
 import valorless.havenbags.HavenBags;
+import valorless.havenbags.Lang;
 import valorless.havenbags.utils.TextFeatures;
 import valorless.valorlessutils.utils.Utils;
 
 public class CommandInfo {
-	
+
 	final static String Name = "§7[§aHaven§bBags§7]§r";
 
 	public static boolean Run(HBCommand command) {
-		
-		ItemStack hand = Bukkit.getPlayer(command.sender.getName()).getInventory().getItemInMainHand();
-		ItemMeta meta = Bukkit.getPlayer(command.sender.getName()).getInventory().getItemInMainHand().getItemMeta();
-			
-		if(HavenBags.IsBag(hand) && BagState.getState(hand) == BagState.USED) {
-			Data data = BagData.GetBag(HavenBags.GetBagUUID(hand), hand);
-			
-			String uuid = data.getUuid();
-			String owner = data.getOwner();
-			String creator = data.getCreator();
-			Boolean canBind = (!owner.equalsIgnoreCase("ownerless")) ? true : false;
-			Integer size = data.getSize();
-			String filter = data.getAutopickup();
-			List<String> trust = new ArrayList<String>();
-			String trusted = "";
-			String weight = "";
-			if(uuid.equalsIgnoreCase("null")) {
-				weight = "0.0";
-			}else {
-				weight = TextFeatures.LimitDecimal(String.valueOf(HavenBags.GetWeight(hand)),2);
-			}
-			String limit = String.valueOf(PDC.GetDouble(hand, "weight-limit").intValue());
-			List<String> lore = meta.getLore();
-			
-			String _lore = "";
-			for(int i = 0; i < lore.size(); i++) { _lore = _lore + "\n    " + lore.get(i); }
-			
-			String info = "§6## HavenBag Bag Information ##";
-			
-			if(!Utils.IsStringNullOrEmpty(uuid)) { info = info + "\n  §fUUID: §e" + uuid; }
-			if(!Utils.IsStringNullOrEmpty(owner)) { 
-				if (canBind) {
+
+		if(command.sender instanceof Player sender) {
+			ItemStack hand = sender.getInventory().getItemInMainHand();
+			ItemMeta meta = sender.getInventory().getItemInMainHand().getItemMeta();
+
+			if(HavenBags.IsBag(hand) && BagState.getState(hand) == BagState.USED) {
+				Data data = BagData.GetBag(HavenBags.GetBagUUID(hand), hand);
+
+				String uuid = data.getUuid();
+				String owner = data.getOwner();
+				String creator = data.getCreator();
+				Boolean canBind = (!owner.equalsIgnoreCase("ownerless")) ? true : false;
+				Integer size = data.getSize();
+				String filter = data.getAutopickup();
+				List<String> trust = new ArrayList<String>();
+				String trusted = "";
+				String weight = "";
+				if(uuid.equalsIgnoreCase("null")) {
+					weight = "0.0";
+				}else {
+					weight = TextFeatures.LimitDecimal(String.valueOf(HavenBags.GetWeight(hand)),2);
+				}
+				String limit = String.valueOf(PDC.GetDouble(hand, "weight-limit").intValue());
+				List<String> lore = meta.getLore();
+
+				String _lore = "";
+				for(int i = 0; i < lore.size(); i++) { _lore = _lore + "\n    " + lore.get(i); }
+
+				command.sender.sendMessage("§6## HavenBag Bag Information ##");
+
+				Message message = new Message(ChatMessageType.CHAT, 
+						"  §fUUID: §e" + uuid + " §7(§eClick to copy§7)"
+						);
+				message.SetHoverText(Lang.Parse("Click to copy.", sender));
+				message.addCopyToClipboardEvent(uuid);
+				message.Send(sender);
+
+				if(!Utils.IsStringNullOrEmpty(data.getTexture())) { 
+					Message texture = new Message(ChatMessageType.CHAT, 
+							"  §fTexture: ... §7(§eClick to copy§7)"
+							);
+					texture.SetHoverText(Lang.Parse( "Base64: " + data.getTexture(), sender));
+					texture.addCopyToClipboardEvent(data.getTexture());
+					texture.Send(sender);
+				}
+
+				List<String> infoList = new ArrayList<String>();
+
+				String info = "";
+				if(!Utils.IsStringNullOrEmpty(owner)) { 
+					if (canBind) {
+						try {
+							Message m_owner = new Message(ChatMessageType.CHAT, 
+									String.format("  §fOwner: §e%s (%s)", Bukkit.getOfflinePlayer(UUID.fromString(owner)).getName(), owner) + " §7(§eClick to copy§7)"
+									);
+							m_owner.SetHoverText(Lang.Parse("Click to copy.", sender));
+							m_owner.addCopyToClipboardEvent(owner);
+							m_owner.Send(sender);
+							//infoList.add(String.format("  §fOwner: §e%s (%s)", Bukkit.getOfflinePlayer(UUID.fromString(owner)).getName(), owner)); 
+						} catch(Exception e) {
+							infoList.add("  §fOwner: §4Error");
+						}
+					}else {
+						infoList.add("  §fOwner: §e" + owner);
+					}
+				}
+				if(!Utils.IsStringNullOrEmpty(creator) && !creator.equalsIgnoreCase("null")) {
 					try {
-						info = info + String.format("\n  §fOwner: §e%s (%s)", Bukkit.getOfflinePlayer(UUID.fromString(owner)).getName(), owner); 
+						info = info + String.format("\n  §fCreator: §e%s (%s)", Bukkit.getOfflinePlayer(UUID.fromString(creator)).getName(), creator);
 					} catch(Exception e) {
-						info = info + "\n  §fOwner: §4Error";
+						infoList.add("  §fCreator: §4Error");
 					}
 				}else {
-					info = info + "\n  §fOwner: §e" + owner;
+					infoList.add("  §fCreator: §enull");
 				}
-			}
-			if(!Utils.IsStringNullOrEmpty(creator) && !creator.equalsIgnoreCase("null")) {
-				try {
-					info = info + String.format("\n  §fCreator: §e%s (%s)", Bukkit.getOfflinePlayer(UUID.fromString(creator)).getName(), creator);
-				} catch(Exception e) {
-					info = info + "\n  §fCreator: §4Error";
+				if(canBind != null) { infoList.add("  §fCanBind: §e" + canBind.toString()); }
+				if(size != null) { infoList.add("  §fSize: §e" + size.toString()); }
+				//if(filter != null) { infoList.add("  §fFilter: §e" + filter; }
+				if(!Utils.IsStringNullOrEmpty(filter)) {
+					infoList.add("  §fFilter: §e" + filter);
+				}else {
+					infoList.add("  §fFilter: §enone");
 				}
-			}else {
-				info = info + "\n  §fCreator: §enull";
-			}
-			if(canBind != null) { info = info + "\n  §fCanBind: §e" + canBind.toString(); }
-			if(size != null) { info = info + "\n  §fSize: §e" + size.toString(); }
-			//if(filter != null) { info = info + "\n  §fFilter: §e" + filter; }
-			if(!Utils.IsStringNullOrEmpty(filter)) {
-				info = info + "\n  §fFilter: §e" + filter;
-			}else {
-				info = info + "\n  §fFilter: §enone";
-			}
-			
-			if(!data.getTrusted().isEmpty()) {
-				trust = data.getTrusted();
-			}
-			if(!trust.isEmpty()) {
-				for(int i = 0; i < trust.size(); i++) { 
-					if(i != 0) {
-						trusted = trusted + ", " + trust.get(i); 
-					}else {
-						trusted = trust.get(i); 
+
+				if(!data.getTrusted().isEmpty()) {
+					trust = data.getTrusted();
+				}
+				if(!trust.isEmpty()) {
+					for(int i = 0; i < trust.size(); i++) { 
+						if(i != 0) {
+							trusted = trusted + ", " + trust.get(i); 
+						}else {
+							trusted = trust.get(i); 
+						}
 					}
+					infoList.add("  §fTrusted: §e" + trusted);
+				}else {
+					infoList.add("  §fTrusted: §enone");
 				}
-				info = info + "\n  §fTrusted: §e" + trusted;
-			}else {
-				info = info + "\n  §fTrusted: §enone";
+				if(weight != null) { infoList.add("  §fWeight: §e" + weight); }
+				if(limit != null) { infoList.add("  §fWeight Limit: §e" + limit); }
+				if(data.getModeldata() != null && data.getModeldata() != 0) { 
+					infoList.add("  §fCustom Model Data: " + data.getModeldata());
+				}
+				if(!Utils.IsStringNullOrEmpty(data.getItemmodel())) { 
+					infoList.add("  §fItemModel: " + data.getItemmodel());
+				}
+				if(data.getBlacklist() != null && !data.getBlacklist().isEmpty()) {
+					infoList.add("  §fBlacklist:");
+					for(String blacked : data.getBlacklist()) {
+						infoList.add("    §f" + blacked);
+					}
+					infoList.add("  §fWhitelist:" + data.isWhitelist());
+				}
+				
+				//if(lore != null) { infoList.add("  §fLore:§r" + _lore); }
+				if(data.getTooltipStyle() != null) { infoList.add("  §fTooltip Style: " + data.getTooltipStyle()); }
+
+				sender.sendMessage(String.join("\n", infoList));
+				return true;
 			}
-			if(weight != null) { info = info + "\n  §fWeight: §e" + weight; }
-			if(limit != null) { info = info + "\n  §fWeight Limit: §e" + limit; }
-			if(lore != null) { info = info + "\n  §fLore:§r" + _lore; }
-			
-			command.sender.sendMessage(info);
-			return true;
 		}
 		return true;
 	}
--- src/main/java/valorless/havenbags/datamodels/Message.java
+++ src/main/java/valorless/havenbags/datamodels/Message.java
@@ -18,9 +18,14 @@ public class Message {
 		this.message = new TextComponent(Lang.Parse(message, null));
 	}
 	
-	public Message(String message, String color) {
+	public Message(String message, ChatColor color) {
 		this.message = new TextComponent(Lang.Parse(message, null));
 		SetColor(color);
+	}
+	
+	public Message(String message, String hover) {
+		this.message = new TextComponent(Lang.Parse(message, null));
+		SetHoverText(this.message, hover);
 	}
 	
 	public Message(ChatMessageType type, String message) {
@@ -28,9 +33,15 @@ public class Message {
 		this.type = type;
 	}
 	
-	public Message(ChatMessageType type, String message, String color) {
+	public Message(ChatMessageType type, String message, ChatColor color) {
 		this.message = new TextComponent(Lang.Parse(message, null));
 		SetColor(color);
+		this.type = type;
+	}
+	
+	public Message(ChatMessageType type, String message, String hover) {
+		this.message = new TextComponent(Lang.Parse(message, null));
+		SetHoverText(this.message, hover);
 		this.type = type;
 	}
 	
@@ -43,14 +54,17 @@ public class Message {
 		}
 	}
 	
-	public void SetColor(String color) {
-		message.setColor(ChatColor.getByChar(color.charAt(0)));
+	public void SetColor(ChatColor color) {
+		message.setColor(color);
 	}
 	
 	@SuppressWarnings("deprecation")
 	TextComponent SetHoverText(TextComponent comp, String message) {
 		comp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(Lang.Parse(message, null)).create()));
 		return comp;
+	}
+	public void SetHoverText(String message) {
+		this.message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(Lang.Parse(message, null)).create()));
 	}
 	
 	public void AddTextPart(String message) {
@@ -71,6 +85,22 @@ public class Message {
 		this.message.addExtra(
 				SetHoverText(new TextComponent("\n" + Lang.Parse(message, null)), hover)
 				);
+	}
+	
+	public void addClickEvent(String command) {
+		message.setClickEvent(new net.md_5.bungee.api.chat.ClickEvent(net.md_5.bungee.api.chat.ClickEvent.Action.RUN_COMMAND, command));
+	}
+	
+	public void addSuggestEvent(String command) {
+		message.setClickEvent(new net.md_5.bungee.api.chat.ClickEvent(net.md_5.bungee.api.chat.ClickEvent.Action.SUGGEST_COMMAND, command));
+	}
+	
+	public void addOpenUrlEvent(String url) {
+		message.setClickEvent(new net.md_5.bungee.api.chat.ClickEvent(net.md_5.bungee.api.chat.ClickEvent.Action.OPEN_URL, url));
+	}
+	
+	public void addCopyToClipboardEvent(String text) {
+		message.setClickEvent(new net.md_5.bungee.api.chat.ClickEvent(net.md_5.bungee.api.chat.ClickEvent.Action.COPY_TO_CLIPBOARD, text));
 	}
 	
 
